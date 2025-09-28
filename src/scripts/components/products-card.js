43d6da002ebc766bc8c15763f8a48a99cbb48b4c
Michal Fryč <michal.fryc@seznam.cz>
2025-09-28 17:00:08 +0200

    Base style for product-list

--- src/scripts/components/products-card.js
+++ src/scripts/components/products-card.js
@@ -1,9 +1,9 @@
 export function ProductsCard(product) {
     const component = document.createElement('div')
 
-    component.className = 'x-product-card'
+    component.className = 'x-products-card'
     component.innerHTML = `
-        <img src="${product.image_url}" alt="${product.name}" />
+        <img class="x-products-card__image-file" src="${product.image_url}" alt="${product.name}" />
         <span>${product.name}</span>
         <span>${product.price}</span>
         <span>${product.currency}</span>
